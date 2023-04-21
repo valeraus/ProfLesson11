620b2f1ba8a22b5cc2b80c53158f50f9b84f4b93
Valera <vlaus0@gmail.com>
2023-04-21 23:13:04 +0300

    Добавьте файлы проекта.

---
+++ AddTask/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace AddTask
+{
+    /*
+    Використовуючи конструкції блокування, змініть останній приклад уроку таким чином, 
+    щоб отримати можливість почергової роботи 3-х потоків.
+     */
+    class Program
+    {
+        static private int counter = 0;
+
+        static private object block = new object();
+
+        static private void Function1()
+        {
+            lock (block)
+            {
+                for (int i = 0; i < 50; ++i)
+                {
+                    Monitor.Wait(block, Timeout.Infinite);
+                    Console.Write("1 ");
+                    Console.WriteLine("{0} из потока {1}", ++counter, Thread.CurrentThread.GetHashCode());
+                    Monitor.Pulse(block);
+                }
+            }
+        }
+
+        static private void Function2()
+        {
+            lock (block)
+            {
+                for (int i = 0; i < 50; ++i)
+                {
+                    Monitor.Wait(block, Timeout.Infinite);
+                    Console.Write("2 ");
+                    Console.WriteLine("{0} из потока {1}", ++counter, Thread.CurrentThread.GetHashCode());
+                    Monitor.Pulse(block);
+                }
+            }
+        }
+
+        static private void Function3()
+        {
+            lock (block)
+            {
+                for (int i = 0; i < 50; ++i)
+                {
+                    Monitor.Pulse(block);
+                    Monitor.Wait(block, Timeout.Infinite);
+                    Console.Write("3 ");
+                    Console.WriteLine("{0} из потока {1}", ++counter, Thread.CurrentThread.GetHashCode());
+                }
+            }
+        }
+
+        static void Main()
+        {
+            Thread[] threads = { new Thread(Function1), new Thread(Function2), new Thread(Function3) };
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Start();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Task2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Threading;
+
+namespace Task2
+{
+    /*
+     Створіть консольний додаток, який в різних потоках зможе отримати доступ до двох файлів. 
+    Зчитайте з цих файлів їх вміст і спробуйте записати отриману інформацію в третій файл. 
+    Читання/запис повинні здійснюватися одночасно в кожному з дочірніх потоків. 
+    Використовуйте блокування потоків для того, щоб домогтися коректного запису в кінцевий файл.
+     */
+    class Program
+    {
+        static readonly StreamReader stream1 = File.OpenText("text1.txt");
+        static readonly StreamReader stream2 = File.OpenText("text2.txt");
+        static readonly StreamWriter stream3 = File.CreateText("text3.txt");
+
+        static object blok = new object();
+
+        static void ReadText1()
+        {
+            string str = stream1.ReadToEnd();
+
+            lock (blok)
+            {
+                stream3.WriteLine(str);
+            }
+        }
+
+        static void ReadText2()
+        {
+            string str = stream2.ReadToEnd();
+
+            lock (blok)
+            {
+                stream3.WriteLine(str);
+            }
+        }
+
+        static void Main()
+        {
+            Thread[] array = new Thread[] { new Thread(ReadText1), new Thread(ReadText2) };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].Start();
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].Join();
+            }
+
+            stream3.Close();
+        }
+    }
+}
